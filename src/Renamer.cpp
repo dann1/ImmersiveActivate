4f1d950af4721071c4dc2c806127c372f22d208f
Styyx <styyx228@gmail.com>
2023-08-06 23:31:39 +0200

    settings expanded

--- src/Renamer.cpp
+++ src/Renamer.cpp
@@ -1,49 +1,52 @@
 #include "Renamer.h"
+#include "Settings.h"
 
+ 
 // TODO: Make name configurable per form type
 std::string GetFormTypeText(const RE::TESObjectREFRPtr& a_object, std::string a_name)
 {
 	const auto a_baseObject = a_object->GetBaseObject();
 	const auto a_formType = a_baseObject->GetFormType();
-
+	const auto settings = Settings::GetSingleton();
+	
 	logger::debug("CrossHair FormType: {}", a_formType);
 
 	switch (a_formType) {
 	// TODO: Refine by keyword. Humanoid, Creature, race, etc.
 	case RE::FormType::NPC:
 	case RE::FormType::LeveledNPC:
-		return "NPC";
+		return settings->npc_show.text;
 	case RE::FormType::Door:
-		return "Door";
+		return settings->door_show.text;
 	case RE::FormType::Activator:
 		return a_name;
 	case RE::FormType::Container:
-		return "Storage";
+		return settings->container_show.text;
 	case RE::FormType::Furniture:
 		return a_name;
 	case RE::FormType::Flora:
 	case RE::FormType::Tree:
-		return "Resource";
-	case RE::FormType::Ingredient:
-		return "Reagent";
+		return settings->resource_show.text;
+	case RE::FormType::Ingredient:							
+		return settings->ingredient_show.text;
 	case RE::FormType::AlchemyItem:
-		return "Provision";
+		return settings->ingredient_show.text;
 	case RE::FormType::Ammo:
-		return "Ammunition";
+		return settings->ammo_show.text;
 	case RE::FormType::Weapon:
-		return "Weapon";
+		return settings->weapon_show.text;
 	case RE::FormType::Armor:
-		return "Equipment";
+		return settings->armor_show.text;
 	case RE::FormType::Scroll:
 	case RE::FormType::Note:
-		return "Document";
+		return settings->scroll_note_show.text;
 	case RE::FormType::Book:
-		return "Book";
+		return settings->book_show.text;
 	case RE::FormType::SoulGem:
-		return "SoulGem";
+		return settings->soulgem_show.text;
 	case RE::FormType::KeyMaster:
-		return "Key";
+		return settings->key_show.text;
 	default:
-		return "Item";
+		return settings->items_show.text;
 	}
 }
