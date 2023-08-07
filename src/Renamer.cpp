915ddb5a2cfc4bc90c571de0bb09df277e59b9da
dann1 <dann1telecom@gmail.com>
2023-08-06 19:39:10 -0600

    More names

--- src/Renamer.cpp
+++ src/Renamer.cpp
@@ -9,10 +9,19 @@ std::string GetFormTypeText(const RE::TESObjectREFRPtr& a_object, std::string a_
 	logger::debug("CrossHair FormType: {}", a_formType);
 
 	switch (a_formType) {
-	// TODO: Refine by keyword. Humanoid, Creature, race, etc.
 	case RE::FormType::NPC:
 	case RE::FormType::LeveledNPC:
-		return "NPC";
+		if (a_object->IsDead()) {
+			return "Corpse";
+		} else if (a_object->IsAnimal()) {
+			return "Animal";
+		} else if (a_object->IsChild()) {
+			return "Child";
+		} else if (a_object->IsDragon()) {
+			return "Dragon";
+		} else {
+			return "Person";
+		}
 	case RE::FormType::Door:
 		return "Door";
 	case RE::FormType::Activator:
@@ -26,6 +35,7 @@ std::string GetFormTypeText(const RE::TESObjectREFRPtr& a_object, std::string a_
 		return "Resource";
 	case RE::FormType::Ingredient:
 		return "Reagent";
+	// TODO: Potion/Poison detection
 	case RE::FormType::AlchemyItem:
 		return "Provision";
 	case RE::FormType::Ammo:
@@ -33,7 +43,11 @@ std::string GetFormTypeText(const RE::TESObjectREFRPtr& a_object, std::string a_
 	case RE::FormType::Weapon:
 		return "Weapon";
 	case RE::FormType::Armor:
-		return "Equipment";
+		if (a_object->IsJewelry()) {
+			return "Jewel";
+		} else {
+			return "Equipment";
+		}
 	case RE::FormType::Scroll:
 	case RE::FormType::Note:
 		return "Document";
@@ -44,6 +58,13 @@ std::string GetFormTypeText(const RE::TESObjectREFRPtr& a_object, std::string a_
 	case RE::FormType::KeyMaster:
 		return "Key";
 	default:
-		return "Item";
+		// TODO: Detect septim as money
+		const auto a_formID = a_object->GetFormID();
+
+		if (a_object->IsLockpick()) {
+			return "Lockpick";
+		} else {
+			return "Item";
+		}
 	}
 }
